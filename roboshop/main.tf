b4c165a0f71559b4ca8461742a13f00d47f2e25b
shiva shankar <sivas1475@gmail.com>
2022-12-18 01:40:49 +0530

    JENKINS

--- roboshop/main.tf
+++ roboshop/main.tf
@@ -1,7 +1,8 @@
-module "frontend" {
+module "common" {
+  count         = length(var.COMPONENTS)
   source        = "./common"
   INSTANCE_TYPE = "t2.micro"
   AMI           = "ami-098178c0950504f72"
-  COMPONENT     = "frontend"
-  PORT          = 80
+  COMPONENT     = var.COMPONENTS[count.index]
+  PORT          = var.PORTS[count.index]
 }
