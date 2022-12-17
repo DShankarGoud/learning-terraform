cc5412b46c3506b5f29249b6542ad72b19b66b8a
shiva shankar <sivas1475@gmail.com>
2022-12-18 00:00:16 +0530

    JENKINS

---
+++ example1/ec2.tf
@@ -0,0 +1,17 @@
+resource "aws_instance" "sample" {
+  instance_type = "t2.micro"
+  ami           = "ami-098178c0950504f72"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "= 4.14.0"
+    }
+  }
+}
