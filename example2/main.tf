c4957fadf5316e2f1dc009aceb81c0ae704f406b
shiva shankar <sivas1475@gmail.com>
2022-12-18 00:57:00 +0530

    JENKINS

--- example2/main.tf
+++ example2/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket = "terraform-b64"
+    bucket = "terraform-s64"
     key    = "example2/terraform.tfstate"
     region = "us-east-1"
   }
