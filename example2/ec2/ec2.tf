28e7f452acb7d22d08dd591d514374edffd838e6
shiva shankar <sivas1475@gmail.com>
2022-12-18 00:52:58 +0530

    JENKINS

--- example2/ec2/ec2.tf
+++ example2/ec2/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "sample" {
-  instance_type          = "t3.small"
-  ami                    = "ami-0eb5f3f64b10d3e0e"
+  instance_type = "t2.micro"
+  ami           = "ami-098178c0950504f72"
   vpc_security_group_ids = [var.SG_ID]
 }
 
